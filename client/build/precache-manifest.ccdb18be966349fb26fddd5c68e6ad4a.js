8028b1888ea55435bb6d2b27505edcf5b75a75ee
Noah Tigner <nzt@cs.uoregon.edu>
2020-08-13 20:51:58 -0700

    added bootstrap

---
+++ client/build/precache-manifest.ccdb18be966349fb26fddd5c68e6ad4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f29bab659c13da5487291b4e328849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b87e9c99bba4c887b21",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "613d166fd0583113f1cb",
+    "url": "/static/css/main.a7c21057.chunk.css"
+  },
+  {
+    "revision": "1b87e9c99bba4c887b21",
+    "url": "/static/js/2.c2916835.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c2916835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613d166fd0583113f1cb",
+    "url": "/static/js/main.f1ec88bb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  }
+]);
