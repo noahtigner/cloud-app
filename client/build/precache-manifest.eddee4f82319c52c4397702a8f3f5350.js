e8572ed5aefd489d42960bc9ba06ee19b6250597
Noah Tigner <nzt@cs.uoregon.edu>
2020-08-22 10:46:53 -0700

    Possibly fixed static file rendering

---
+++ client/build/precache-manifest.eddee4f82319c52c4397702a8f3f5350.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcd846ae28ac293cdd798a157f7f2b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f33906d7b185272b8b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "eb305562b8e5947f188f",
+    "url": "/static/css/main.04dd6de5.chunk.css"
+  },
+  {
+    "revision": "b9f33906d7b185272b8b",
+    "url": "/static/js/2.af598ba1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.af598ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb305562b8e5947f188f",
+    "url": "/static/js/main.42848874.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  }
+]);
